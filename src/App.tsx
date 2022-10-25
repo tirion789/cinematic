3c92f151514637c98906ae155af0809afe881198
tirion <tirionchen@mail.ru>
2022-10-25 04:22:40 +0300

    add new

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Home from './pages/home';
 import Main from './pages/main';
 
 import './scss/app.scss';
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './header.module.scss';
 
 const Header: React.FC = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className={styles.header}>
       <ul className={styles.header__list}>
         <li className={styles.header__name}>CINETREX</li>
         <li className={styles.header__buttons_list}>
-          <button className={styles.header__buttons}>Sign in</button>
-          <button className={styles.header__buttons}>Join</button>
+          <a onClick={() => setOpen(true)} className={styles.header__buttons}>
+            Sign in
+          </a>
+          <a className={styles.header__buttons}>Join</a>
         </li>
       </ul>
+      <div className={open ? styles.header__modal_overlay : ''}>
+        <div className={`${styles.header__modal} ${open ? styles.header__modal_show : ''}`}>
+          <div className={styles.header__modal_inner}>
+            <button className={styles.header__close} onClick={() => setOpen(false)}></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
